766b4798fd4443616f5da5cb3b7035f1d6d2a36c
Brian Mara <brianmara123@gmail.com>
2024-06-10 19:15:03 +0800

    Adding forgot password form

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,23 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../views/Dashboard.vue";
 import Login from "../views/Login.vue";
+import ForgotPassword from "../views/ForgotPassword.vue";
 
 const routes = [
-    {
-        path: "/dashboard",
-        name: "dashboard",
-        component: Dashboard,
-    },
-    {
-        path: "/login",
-        name: "login",
-        component: Login,
-    },
+  {
+    path: "/dashboard",
+    name: "dashboard",
+    component: Dashboard,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/forgot-password",
+    name: "forgot-password",
+    component: ForgotPassword,
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
