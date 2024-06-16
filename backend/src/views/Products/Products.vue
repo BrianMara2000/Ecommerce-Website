5d6572f0a8d784a8b3fbce5a0b2b7c1f171d2a8a
Brian Mara <brianmara123@gmail.com>
2024-06-16 14:45:57 +0800

    Creating empty product modal

--- backend/src/views/Products/Products.vue
+++ backend/src/views/Products/Products.vue
@@ -2,15 +2,26 @@
   <div class="flex items-center justify-between mb-3">
     <h1 class="text-3xl font-semibold">Products</h1>
     <button
-      type="submit"
-      class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+      @click="showProductModal"
+      type="button"
+      class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 cursor-pointer"
     >
       Add new product
     </button>
   </div>
+
+  <ProductModal v-model="showModal" />
   <ProductsTable />
 </template>
 
 <script setup>
+import { ref } from "vue";
 import ProductsTable from "./ProductsTable.vue";
+import ProductModal from "./ProductModal.vue";
+
+const showModal = ref(false);
+
+function showProductModal() {
+  showModal.value = true;
+}
 </script>
