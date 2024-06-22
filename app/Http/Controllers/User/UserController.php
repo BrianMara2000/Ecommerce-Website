fff6d33f306c284336bae557c1abb9d26aff3e2a
Brian Mara <brianmara123@gmail.com>
2024-06-22 16:12:49 +0800

    Home page UI set up

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,15 +3,20 @@
 namespace App\Http\Controllers\User;
 
 use Inertia\Inertia;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Application;
+use App\Http\Resources\ProductListResource;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $products = Product::query()->orderBy('id', 'desc')->paginate(5);
+
         return Inertia::render('User/Index', [
+            'products' => $products,
             'canLogin' => app('router')->has('login'),
             'canRegister' => app('router')->has('register'),
             'laravelVersion' => Application::VERSION,
