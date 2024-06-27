41ec4f72c91cc4dac948af47e96d6003ecd7f89c
Brian Mara <brianmara123@gmail.com>
2024-06-28 02:13:20 +0800

    Preparing API routes for cart management

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,24 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\User\CartController;
 // use App\Http\Controllers\User\CartController;
 use App\Http\Controllers\User\ProductController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [ProductController::class, 'index'])->name('user.home');
-Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product.view');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['guestOrVerified'])->group(function () {
+    Route::get('/', [ProductController::class, 'index'])->name('user.home');
+    Route::get('/product/{product:slug}', [ProductController::class, 'show'])->name('product.view');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::prefix('/cart')->name('cart.')->group(function () {
+        Route::get('/', [CartController::class, 'index'])->name('index');
+        Route::get('/add/{product:slug}', [CartController::class, 'add'])->name('add');
+        Route::get('/remove/{product:slug}', [CartController::class, 'remove'])->name('remove');
+        Route::get('/update-quantity/{product:slug}', [CartController::class, 'updateQuantity'])->name('updateQuantity');
+    });
 });
 
 // Route::prefix('cart')->controller(CartController::class)->group(function () {
