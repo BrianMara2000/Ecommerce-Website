7731cb734e8cd81eca879e77f6a7e8fcc7311fa2
Brian Mara <brianmara123@gmail.com>
2024-07-11 11:08:46 +0800

    Change directory - app/helper/cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Helpers\Cart;
+use App\Helpers\Cart;
 use App\Models\CartItem;
 use App\Models\Product;
 use Illuminate\Http\Request;
