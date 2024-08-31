8c8c023a9b79a51e334c7ceeea0f722a07ff0c9c
Brian Mara <brianmara123@gmail.com>
2024-08-31 21:25:38 +0800

    Apply toast library to the parent element so that it will render to the children element

--- resources/js/Pages/User/Layouts/UserLayout.vue
+++ resources/js/Pages/User/Layouts/UserLayout.vue
@@ -9,7 +9,14 @@ import { onMounted } from "vue";
 import { initFlowbite } from "flowbite";
 import Header from "./Header.vue";
 import Footer from "./Footer.vue";
+import { usePage } from "@inertiajs/vue3";
+import { toast } from "vue3-toastify";
+
 onMounted(() => {
   initFlowbite();
+  if (usePage().props.flash.info) {
+    toast.info(usePage().props.flash.info);
+  } else {
+  }
 });
 </script>
