b61a01832bfee1ea4344f3cf270e8a7a362f69a2
Brian Mara <brianmara123@gmail.com>
2024-09-02 13:04:23 +0800

    Setup eloquent model relations from customer to addresses and to User

--- app/Models/CustomerAddress.php
+++ app/Models/CustomerAddress.php
@@ -2,10 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class CustomerAddress extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['type', 'address1', 'address2', 'city', 'state', 'zipcode', 'country_code', 'customer_id'];
+
+    public function customer(): HasOne
+    {
+        return $this->hasOne(Customer::class, 'user_id', 'customer_id');
+    }
 }
