76c8322e5822c766506dab7d94c8dce056af9121
Brian Mara <brianmara123@gmail.com>
2024-11-16 12:01:49 +0800

    Change input type into select for country and states

--- resources/js/Components/TextInput.vue
+++ resources/js/Components/TextInput.vue
@@ -1,26 +1,42 @@
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref } from "vue";
 
 const model = defineModel({
+  type: String,
+});
+
+const props = defineProps({
+  type: {
     type: String,
     required: true,
+  },
 });
 
 const input = ref(null);
 
 onMounted(() => {
-    if (input.value.hasAttribute('autofocus')) {
-        input.value.focus();
-    }
+  if (input.value.hasAttribute("autofocus")) {
+    input.value.focus();
+  }
 });
 
 defineExpose({ focus: () => input.value.focus() });
 </script>
 
 <template>
-    <input
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
-        v-model="model"
-        ref="input"
-    />
+  <select
+    v-if="props.type === 'select'"
+    class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+    ref="input"
+    v-model="model"
+  >
+    <slot></slot>
+  </select>
+  <input
+    v-else
+    :type="props.type"
+    class="w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+    v-model="model"
+    ref="input"
+  />
 </template>
