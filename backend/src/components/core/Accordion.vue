04d75dc9f6a16cdf7caf760a16290bc203989604
Brian Mara <brianmara123@gmail.com>
2025-01-08 13:37:42 +0800

    Add Accordion component and OrderStatus component for improved order management UI

---
+++ backend/src/components/core/Accordion.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="bg-white rounded-lg border">
+    <div
+      class="flex justify-between items-center hover:bg-gray-100 p-5 cursor-pointer border-b"
+      @click="toggleAccordion"
+    >
+      <h2 class="text-lg font-semibold">{{ title }}</h2>
+      <button>
+        <ChevronDownIcon
+          :class="{ 'rotate-180': isOpen }"
+          class="h-6 w-6 transition-transform duration-300"
+        />
+      </button>
+    </div>
+    <div v-if="isOpen" class="flex flex-col p-8 justify-center gap-3">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { ChevronDownIcon } from "@heroicons/vue/24/outline";
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+
+const isOpen = ref(false);
+
+const toggleAccordion = () => {
+  isOpen.value = !isOpen.value;
+};
+</script>
+
+<style scoped>
+/* Optional additional styles */
+</style>
---
+++ backend/src/views/Orders/OrderStatus.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="w-[80%] text-sm rounded-md px-1 py-2 flex items-center justify-center"
+    :class="{
+      'bg-emerald-500 text-white': order === 'paid',
+      'bg-yellow-500 text-white': order === 'unpaid',
+      'bg-gray-500 text-white': order === 'shipped',
+      'bg-purple-500 text-white': order === 'completed',
+      'bg-red-500 text-white': order === 'cancelled',
+    }"
+  >
+    {{ capitalizedStatus(order) }}
+  </div>
+</template>
+
+<script setup>
+import { useCapitalizedStatus } from "../../utils/utils";
+
+defineProps({
+  order: String,
+});
+
+const capitalizedStatus = (stats) => useCapitalizedStatus(stats);
+</script>
