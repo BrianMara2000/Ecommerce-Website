ee76fd5e5d8435a98b8bb29cddcd3986eedc506a
Brian Mara <brianmara123@gmail.com>
2025-02-12 22:01:40 +0800

    Update import path for Dashboard component in router

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppLayout from "../components/AppLayout.vue";
-import Dashboard from "../views/Dashboard.vue";
+import Dashboard from "../views/Dashboard/Dashboard.vue";
 import Login from "../views/Login.vue";
 import ForgotPassword from "../views/ForgotPassword.vue";
 import ResetPassword from "../views/ResetPassword.vue";
