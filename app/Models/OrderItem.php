bde15101cbfd23ab1a3b05edbf3cf8a33baaef44
Brian Mara <brianmara123@gmail.com>
2025-02-12 21:58:12 +0800

    Update product relationship in OrderItem model to include trashed products

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -21,6 +21,6 @@ class OrderItem extends Model
 
     public function product(): BelongsTo
     {
-        return $this->belongsTo(Product::class, 'product_id', 'id');
+        return $this->belongsTo(Product::class, 'product_id', 'id')->withTrashed();
     }
 }
